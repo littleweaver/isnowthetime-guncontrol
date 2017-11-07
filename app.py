d02e1afd5f2309678dfbd6ca7e996bc961da38a9
Stephen Burrows <stephen.r.burrows@gmail.com>
2017-11-07 10:11:48 -0800

    Added formatting of current time on the server

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+import datetime
+
+from babel.dates import format_date, format_datetime
 from flask import Flask, render_template
+from pytz import timezone, utc
+
 app = Flask(__name__)
+
+los_angeles_tz = timezone('America/Los_Angeles')
 
 @app.route("/")
 def hello():
-    return render_template('hello.html')
+    utc_dt = datetime.datetime.now(utc)
+    timestamp = utc_dt.astimezone(los_angeles_tz)
+    return render_template(
+        'hello.html',
+        date=format_date(timestamp.date(), 'L/d/YY'),
+        time=format_datetime(timestamp, 'h:mm:ss a z'),
+    )
