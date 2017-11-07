c675cdc3a3f3a98ac5f7d53b3d6d1b8c95fb64db
Stephen Burrows <stephen.r.burrows@gmail.com>
2017-11-06 18:36:10 -0800

    Initial working version of the site

---
+++ static/js/main.js
@@ -0,0 +1,46 @@
+var loadingDelay1 = 1500;
+var loadingDelay2 = 500;
+var results = [
+	'Yes',
+	'Definitely',
+	'Fuck yes',
+	'yes!!1!',
+	'Signs point to "yes"',
+
+];
+
+function displayLoading1() {
+	$('#loadingWrapper').removeClass('hidden');
+	$('#loading1').removeClass('hidden');
+	setTimeout(function() {
+		$('#loading1 .done').removeClass('hidden');
+		setTimeout(displayLoading2, loadingDelay2);
+	}, loadingDelay1);
+}
+
+function displayLoading2() {
+	$('#loading2').removeClass('hidden');
+	setTimeout(function() {
+		$('#loading2 .done').removeClass('hidden');
+		setTimeout(displayLoading3, loadingDelay2);
+	}, loadingDelay1);
+}
+
+function displayLoading3() {
+	$('#loading3').removeClass('hidden');
+	setTimeout(function() {
+		$('#loading3 .done').removeClass('hidden');
+		setTimeout(displayResult, loadingDelay2);
+	}, loadingDelay1);
+}
+
+function displayResult() {
+	var result = results[Math.floor(Math.random()*results.length)];
+	$('#result').removeClass('hidden')
+	$('#resultInner').html(result);
+}
+
+
+$(window).on('load', function() {
+	displayLoading1()
+});
